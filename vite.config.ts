c337492c97f98ea087149f6cba7932b2b4cf160e
Jholo Aviado <jholo.aviado@hoolisoftware.com>
2024-07-29 11:39:34 +0800

    add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'prompt',
+			includeAssets: ['vite.svg'],
+			manifest: {
+				name: 'PowerBI-React-POC',
+				short_name: 'powerbi-react-poc',
+				description: 'I am a simple POC app',
+				theme_color: '#171717',
+				background_color: '#f0e7db',
+				display: 'standalone',
+				scope: '/',
+				start_url: '/',
+				orientation: 'portrait',
+			},
+		}),
+	],
+});
