4cc55d4c61dfceb6af196c5d6799631c446d1723
Jholo Aviado <jholo.aviado@hoolisoftware.com>
2024-07-29 11:59:00 +0800

    add icons for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,14 @@ export default defineConfig({
 	plugins: [
 		react(),
 		VitePWA({
-			registerType: 'prompt',
+			registerType: 'autoUpdate',
 			includeAssets: ['vite.svg'],
+			devOptions: {
+				enabled: true,
+			},
 			manifest: {
 				name: 'PowerBI-React-POC',
-				short_name: 'powerbi-react-poc',
+				short_name: 'PowerBI-React-POC',
 				description: 'I am a simple POC app',
 				theme_color: '#171717',
 				background_color: '#f0e7db',
@@ -19,6 +22,32 @@ export default defineConfig({
 				scope: '/',
 				start_url: '/',
 				orientation: 'portrait',
+				icons: [
+					{
+						src: '/android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'favicon',
+					},
+					{
+						src: '/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'favicon',
+					},
+					{
+						src: '/apple-touch-icon.png',
+						sizes: '180x180',
+						type: 'image/png',
+						purpose: 'apple touch icon',
+					},
+					{
+						src: '/maskable_icon.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
 			},
 		}),
 	],
